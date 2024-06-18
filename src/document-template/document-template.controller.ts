87cb4f9ef633d6942e8f37c16769328ef582cd59
Ozgur Cokceken <ozgur.cokceken@octet.com.tr>
2024-06-18 19:35:30 +0300

    controllers created

--- src/document-template/document-template.controller.ts
+++ src/document-template/document-template.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import {
+  IDocumentAnadoluBank,
+  IDocumentDefault,
+  IDocumentIsBankasi,
+} from './types/abf';
 
-@Controller('document-template')
-export class DocumentTemplateController {}
+@Controller('document-template/abf')
+export class DocumentTemplateController {
+  @Post('default')
+  createDefault(@Body() document: IDocumentDefault) {
+    return document;
+  }
+  @Post('anadolubank')
+  createAnadolubank(@Body() document: IDocumentAnadoluBank) {
+    return document;
+  }
+
+  @Post('isbankasi')
+  createIsbankasi(@Body() document: IDocumentIsBankasi) {
+    return document;
+  }
+}
